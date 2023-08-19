11dff6e18db85f1a0e100151d0558fab5905a94f
Paul Rezzonico Ubuntu <paul.rezzo@ubuntu.com>
2023-08-19 23:21:45 +0200

    refact(Task)remove possibility to generate description by IA

--- app/src/main/java/fr/unilim/stodolist/ui/AddTaskFragment.kt
+++ app/src/main/java/fr/unilim/stodolist/ui/AddTaskFragment.kt
@@ -30,11 +30,6 @@ import kotlinx.serialization.json.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
-private const val OPENAI_API_KEY = "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"
-// sera supprimé dans quelques semaines
-// limité à 1$ (y a de la marge)
-
 class AddTaskFragment : Fragment() {
 
     private var _binding: FragmentAddTaskBinding? = null
@@ -90,7 +85,6 @@ class AddTaskFragment : Fragment() {
 
     private fun saveTask() {
         binding.btnSaveTask.isEnabled = false
-        binding.IADescriptionToggle.isEnabled = false
         binding.btnPickDate.isEnabled = false
         binding.etTaskTitle.isEnabled = false
 
@@ -116,92 +110,23 @@ class AddTaskFragment : Fragment() {
 
         var resultText: String? = null
 
-        if (binding.IADescriptionToggle.isChecked) {
-            CoroutineScope(Dispatchers.IO).launch {
-                val httpClient = HttpClient(CIO) {
-                    install(JsonFeature) {
-                        serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
-                            ignoreUnknownKeys = true
-                        })
-                    }
-                }
-
-                val data = buildJsonObject {
-                    put("model", "text-davinci-003")
-                    put(
-                        "prompt",
-                        "Title of the task: --- $taskTitle --- // 3 very shorts bullet points dividing the tasks to be done, in french, without numbering (use •): "
-                    )
-                    put("max_tokens", 200)
-                }
+        val task = Task(
+            title = taskTitle,
+            status = TaskStatus.TODO,
+            description = resultText,
+            dueDate = dueDate
+        )
 
 
-
-                try {
-                    val response: JsonObject =
-                        httpClient.post("https://api.openai.com/v1/completions") {
-                            headers {
-                                append("Content-Type", "application/json")
-                                append("Authorization", "Bearer $OPENAI_API_KEY")
-                            }
-                            body = data
-                        }
-
-                    resultText = response["choices"]
-                        ?.jsonArray
-                        ?.get(0)
-                        ?.jsonObject
-                        ?.get("text")
-                        ?.jsonPrimitive
-                        ?.content
-                        ?.replace("\"", "")
-
-                    println(resultText)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    resultText = "Indisponible"
-                } finally {
-                    httpClient.close()
-                }
-
-                val task = Task(
-                    title = taskTitle,
-                    status = TaskStatus.TODO,
-                    description = resultText,
-                    dueDate = dueDate
-                )
-
-
-                taskViewModel.insertTask(task)
-                activity?.runOnUiThread {
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.task_added),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    taskViewModel.scheduleTaskNotification(requireContext(), task)
-                    activity?.onBackPressed()
-                }
-            }
-        } else {
-            val task = Task(
-                title = taskTitle,
-                status = TaskStatus.TODO,
-                description = resultText,
-                dueDate = dueDate
-            )
-
-
-            taskViewModel.insertTask(task)
-            activity?.runOnUiThread {
-                Toast.makeText(
-                    requireContext(),
-                    getString(R.string.task_added),
-                    Toast.LENGTH_SHORT
-                ).show()
-                taskViewModel.scheduleTaskNotification(requireContext(), task)
-                activity?.onBackPressed()
-            }
+        taskViewModel.insertTask(task)
+        activity?.runOnUiThread {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.task_added),
+                Toast.LENGTH_SHORT
+            ).show()
+            taskViewModel.scheduleTaskNotification(requireContext(), task)
+            activity?.onBackPressed()
         }
     }
 
